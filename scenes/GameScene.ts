3816f8182a1ca24713d33040b30dd30b02cea921
Jormzay <jonathan.rehn@aalto.fi>
2019-11-15 22:29:17 +0200

    added music playback, starting automatically at load time and looping forever

--- scenes/GameScene.ts
+++ scenes/GameScene.ts
@@ -1,10 +1,11 @@
 import Phaser from 'phaser';
 import { Player } from '../Player';
 import PlayerSprite from '../assets/player.png';
+import Mozart from '../assets/audio/mozart_einekleine.mp3';
 
 export class GameScene extends Phaser.Scene {
   players: Player[] = [];
-
+  music?: Phaser.Sound.BaseSound;
   constructor() {
     super({});
   }
@@ -14,12 +15,26 @@ export class GameScene extends Phaser.Scene {
       frameWidth: 128,
       frameHeight: 128,
     });
+
+    this.load.audio('music', Mozart);
   }
 
   public create() {
     this.cameras.main.setBackgroundColor('#fff');
 
     this.players = [new Player(this)];
+
+    this.music = this.sound.add('music', {
+      mute: false,
+      volume: 1,
+      rate: 1.33,
+      detune: 0,
+      seek: 0,
+      loop: true,
+      delay: 0,
+    });
+
+    this.music.play();
   }
 
   public update() {
