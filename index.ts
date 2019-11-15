362185c6031480df96a5d153fa90c1884efb80c9
Rasmus Eskola <fruitiex@gmail.com>
2019-11-15 17:11:39 +0200

    phaser hello world

--- index.ts
+++ index.ts
@@ -1,1 +1,16 @@
-console.log('hello world')
+import Phaser from 'phaser';
+
+class SimpleGame {
+  game: Phaser.Game;
+
+  constructor() {
+    this.game = new Phaser.Game({
+      width: 1920,
+      height: 1080
+    });
+  }
+}
+
+window.onload = () => {
+  const game = new SimpleGame();
+};
